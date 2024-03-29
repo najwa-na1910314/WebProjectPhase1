7b9db419624cc78445b0f1a3db6f2ffbedb71373
LM10NZ <nz1912964@qu.edu.qa>
2024-03-29 15:27:45 +0300

    update

--- The Book Hub/js/bookDetails.js
+++ The Book Hub/js/bookDetails.js
@@ -3,10 +3,14 @@ document.addEventListener("DOMContentLoaded", async () => {
     var bookId = window.location.href.split("=")[1]
     
     if (bookId != null || bookId != "") {
-        let booklist = localStorage.getItem("books")
+        
+        //let bookIndex = findBookIndex(bookId)
+        //let bookIndex = booklist.findIndex((book)=>book["_id"]==bookId)
+        //console.log(bookIndex)
+        let booklist = JSON.parse( localStorage.getItem("books"))
         let bookIndex = booklist.findIndex((book)=>book["_id"]==bookId)
         if(bookIndex != -1){
-            booklist[bookIndex]
+            fillData(booklist[bookIndex])
         }
         else {
             console.log("No book data");
@@ -16,9 +20,21 @@ document.addEventListener("DOMContentLoaded", async () => {
     }
   });
 
+  function findBookIndex(bookId){
+    let booklist = JSON.parse( localStorage.getItem("books"))
+    let bookIndex = booklist.findIndex((book)=>book["_id"]==bookId)
+    console.log(bookIndex)
+    return bookIndex
+  }
   function fillData(book){
+    let user_data = JSON.parse(localStorage.getItem("user_data"))
      document.getElementById("book_id").value=book["_id"]
     document.getElementById("book_title").value=book["title"]
      document.getElementById("book_quantity").value=book["quantity"]
-     document.getElementById("book_price")
+     document.getElementById("book_price").value=book["price"]
+
+     document.getElementById("city").value=user_data["shippingAddress"]["city"]
+    document.getElementById("zone").value=user_data["shippingAddress"]["zone"]
+     document.getElementById("shipping-address").value=user_data["shippingAddress"]["streetNo"]
+     document.getElementById("buildingNO").value=user_data["shippingAddress"]["buildingNo"]
   }
