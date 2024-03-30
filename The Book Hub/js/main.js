54e5a9c656f405fb98492b6d47f88be1b98e3526
Najwa Al-Majid <123901766+najwa-na1910314@users.noreply.github.com>
2024-03-31 02:53:04 +0300

    updated

--- The Book Hub/js/main.js
+++ The Book Hub/js/main.js
@@ -1,23 +1,47 @@
-document.addEventListener('DOMContentLoaded', async () => {
+document.addEventListener("DOMContentLoaded", async () => {
+  //completed
+
   if (!localStorage.books) {
-      await fetchBooks();
+    await fetchBooks();
   } else {
-      showBooks();
+    showBooks();
   }
-
-  const addBooksForm = document.querySelector("#add_book_form");
-
-  addBooksForm.addEventListener('submit', async (e) => {
-      e.preventDefault();
-      const book = formToObject(e.target);
-      books.unshift(book);
-      localStorage.books = JSON.stringify(books);
-      showBooks();
-      addBooksForm.reset();
-  });
 });
+document.querySelector(".search-button").addEventListener("click", (event) => {
+  //completed
 
-let books = []; // empty array to store added books
+  event.preventDefault(); // Prevent form submission
+  const searchQuery = document.querySelector(".search-box").value.toLowerCase();
+  if (searchQuery !== null || searchQuery !== "") {
+    filterBooks(searchQuery);
+  } else {
+    window.location.href = "index.html";
+  }
+});
+async function filterBooks(searchQuery) {
+  //completed
+
+  let booklist = localStorage.getItem("books");
+  if (booklist != null) {
+    booklist = JSON.parse(localStorage.getItem("books"));
+    let result = booklist.filter((book) =>
+      book["title"].toLowerCase().includes(searchQuery)
+    );
+    console.log(result);
+    await showFilteredBooks(result);
+  } else {
+    console.log("booklist not found");
+  }
+}
+async function showFilteredBooks(booklist) {
+  let ele = document.querySelector(".books");
+  ele.innerHTML = "";
+  booklist.forEach((book) => {
+    ele.innerHTML += toHTML(book);
+  });
+}
+
+let books = []; //empty array to store added books
 
 async function loadPage(pageUrl) {
   await page(pageUrl);
@@ -28,33 +52,35 @@ function formToObject(form) {
   const data = {};
 
   for (const [key, value] of formData) {
-      data[key] = value;
+    data[key] = value;
   }
   return data;
 }
-
 async function fetchBooks() {
+  //completed
+
   try {
-      const response = await fetch("json/books.json");
-      if (!response.ok) {
-          throw new Error("Failed to fetch books");
-      }
-      books = await response.json();
-      console.log(books);
+    const response = await fetch("json/books.json");
+    if (!response.ok) {
+      throw new Error("Failed to fetch books");
+    }
+    books = await response.json();
+    console.log(books);
 
-      books.forEach((book) => {
-          document.getElementById("books").innerHTML += toHTML(book);
-      });
+    books.forEach((book) => {
+      document.getElementById("books").innerHTML += toHTML(book);
+    });
 
-      localStorage.books = JSON.stringify(books);
+    localStorage.books = JSON.stringify(books);
   } catch (error) {
-      console.error("Error fetching books:", error);
+    console.error("Error fetching books:", error);
   }
 }
 
 function showBooks() {
+  //completed
   books = JSON.parse(localStorage.books);
-  const booksHTML = books.map((book) => toHTML(book)).join("");
+  const booksHTML = books.map((r) => toHTML(r)).join(" ");
   document.querySelector(".books").innerHTML = booksHTML;
 }
 
@@ -65,37 +91,30 @@ async function page(pageUrl) {
   mainContent.innerHTML = pageHTMLContent;
 }
 
-function toHTML(book) {
-  return `
-  <h2>Sell a Book:</h2>
-  <form id="add_book_form">
-      <div class="grid-container">
-          <label for="price">Price:</label>
-          <input type="text" id="price" placeholder="Enter Price" required />
-          <label for="quantity">Quantity:</label>
-          <input type="number" id="quantity" placeholder="Enter Quantity" required />
-          <label for="isbn">isbn:</label>
-          <input type="text" id="isbn" placeholder="Enter Book isbn" required />
-          <label for="title">Title:</label>
-          <input type="text" id="title" placeholder="Enter Book Name" required />
-          <label for="categories">categories:</label>
-          <input type="text" id="categories" placeholder="Enter Book categories" required />
-          <label for="status">status:</label>
-          <input type="text" id="status" placeholder="Enter Book status" required />
-          <label for="image">Image:</label>
-          <input type="text" id="image" placeholder="Enter Image URL" required />
-          <input type="submit" value="Add Book" id="add" />
-      </div>
-  </form>
-  `;
-}
-
 async function buyBook(bookId) {
   const purchasePageUrl = "bookDetails.html";
-  if (localStorage.getItem("user_data") != null) {
-      // Redirect to the purchase page
-      window.location.href = purchasePageUrl + "?bookId=" + bookId;
-  } else {
-      console.log("You are not logged in.");
+  if(localStorage.getItem("user_data") != null){
+    // Redirect to the purchase page
+    window.location.href = purchasePageUrl+"?bookId="+bookId; 
+  } 
+  else{
+    console.log("You are not logged in.");
   }
+  
+}
+
+function toHTML(book) {
+  //completed
+  return `
+        <div class="book-card">
+        <img src="${book.thumbnailUrl}" class="card-image"></img>
+        <h7>Title: ${book.title}</h7>
+        <br>
+        <h7>Author(s): ${book.authors}</h7>
+        <br>
+        <h7>Price: ${book.price}$</h7>
+        <br>
+        <button onclick="buyBook('${book["_id"]}')">BUY NOW</button>
+        </div>
+    `;
 }
